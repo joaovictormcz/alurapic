5c2f85e0a855a4e6f9b1d74673b111159ad9182b
João Victor <joaovictor.mcz@gmail.com>
2021-05-08 19:58:23 -0300

    Fim da aula photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {    
+export class AppComponent {   
+
+  photos = [
+    {
+      url:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Okonjima_Lioness.jpg/420px-Okonjima_Lioness.jpg',
+      description:'Leoa'
+    },
+    {
+      url:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Lion_waiting_in_Namibia.jpg/420px-Lion_waiting_in_Namibia.jpg',
+      description:"Leão"
+    },
+    {
+      url:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Lion_waiting_in_Namibia.jpg/420px-Lion_waiting_in_Namibia.jpg',
+      description:"Leão"
+    }
+  ];
+
 }
